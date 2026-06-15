b99518e09cc1e98d92ac54345a24fe18ba671a05
Palentum <79781425+Palentum@users.noreply.github.com>
2026-06-16 03:53:58 +0800

    fix(deploy): handle missing pm2 process

--- scripts/deployBuild.ts
+++ scripts/deployBuild.ts
@@ -3,10 +3,10 @@ import { execSync } from 'child_process'
 try {
   console.log('Executing the commands...')
 
-  execSync('git pull && pnpm build && pnpm stop && pnpm start', {
+  execSync('git pull && pnpm build && pnpm start', {
     stdio: 'inherit'
   })
 } catch (error) {
-  console.error('Invalid environment variables', error)
+  console.error('Deploy build failed', error)
   process.exit(1)
 }
